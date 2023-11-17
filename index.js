86eed14e95ea79ba4014c710c6ae4920dc793c18
CrazyJMB <crazyjmbemail@gmail.com>
2023-11-17 23:16:46 +0100

    Creacion de la API de Cataluña

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const csvToJson = require("convert-csv-to-json");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.text());
+
+app.post("/jsonFromBody", (req, res) => {
+  const filePath = "tempFile.csv";
+  fs.writeFileSync(filePath, req.body);
+
+  try {
+    const dataJson = csvToJson.fieldDelimiter(";").getJsonFromCsv(filePath);
+    fs.unlinkSync(filePath);
+
+    res.json(dataJson);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/jsonFromFile", (req, res) => {
+  try {
+    const filePath = "data/centros.csv";
+    const dataJson = csvToJson.fieldDelimiter(";").getJsonFromCsv(filePath);
+    res.json(dataJson);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
